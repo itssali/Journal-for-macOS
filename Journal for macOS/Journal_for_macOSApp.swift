8ba7cc9f7a1b99396cb2abe40d88e36a0c0c8bc2
itssali <woowooch123@gmail.com>
2024-11-01 13:42:11 +0200

    Initial Commit

---
+++ Journal for macOS/Journal_for_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_for_macOSApp.swift
+//  Journal for macOS
+//
+//  Created by Ali Nasser on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Journal_for_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
