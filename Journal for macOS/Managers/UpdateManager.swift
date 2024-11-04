e5f0907cec41a421868b433d0f427e3c7673c83c
itssali <woowooch123@gmail.com>
2024-11-04 15:54:47 +0200

    1.0.2 test update

--- Journal for macOS/Managers/UpdateManager.swift
+++ Journal for macOS/Managers/UpdateManager.swift
@@ -4,26 +4,25 @@ import SwiftUI
 class UpdateManager: NSObject, ObservableObject {
     private var updater: SPUUpdater
     private var automaticCheckEnabled: Bool
+    private var controller: SPUStandardUpdaterController
     
     @Published var canCheckForUpdates = false
     
     override init() {
-        let controller = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+        // Create the updater controller with self as the delegate
+        controller = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
         updater = controller.updater
         automaticCheckEnabled = true
         
         super.init()
+        
+        // Configure the updater
+        updater.publisher(for: \.canCheckForUpdates)
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$canCheckForUpdates)
     }
     
     func checkForUpdates() {
         updater.checkForUpdates()
-    }
-}
-
-extension UpdateManager: SPUUpdaterDelegate {
-    func updater(_ updater: SPUUpdater, didFinishLoading appcast: SUAppcast) {
-        DispatchQueue.main.async {
-            self.canCheckForUpdates = true
-        }
     }
 }
