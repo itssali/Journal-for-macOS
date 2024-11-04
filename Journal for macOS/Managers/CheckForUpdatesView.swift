9d51e4b687f9a4754b9ad36988c148cf1f17f4a2
itssali <woowooch123@gmail.com>
2024-11-04 21:30:18 +0200

    back to normal

---
+++ Journal for macOS/Managers/CheckForUpdatesView.swift
@@ -0,0 +1,34 @@
+//
+//  Update.swift
+//  Journal for macOS
+//
+//  Created by Ali Nasser on 04/11/2024.
+//
+
+import SwiftUI
+import Sparkle
+
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+}
